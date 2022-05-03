59bd103c6d95db8170e635e32ac8ed724329d8da
Nico De Witte <nico.dewitte@outlook.com>
2022-05-03 20:12:05 +0200

    Demo of feathers website

--- src/models/posts.model.ts
+++ src/models/posts.model.ts
@@ -6,36 +6,61 @@ import { Application } from '../declarations';
 import { Model, Mongoose } from 'mongoose';
 
 export default function (app: Application): Array<Model<any>> {
-  const modelName = 'posts';
+  // const modelName = 'posts';
   const mongooseClient: Mongoose = app.get('mongooseClient');
   const { Schema } = mongooseClient;
 
-  // Generic Post Schema
+
+  const options = {
+    discriminatorKey: '_type'
+  };
+
   const PostSchema = new Schema({
-    text: { type: String, required: true }
-  }, {
-    discriminatorKey: '_type',
-    timestamps: true,
-  });
+    createdAt: { type: Date, 'default': Date.now },
+    updatedAt: { type: Date, 'default': Date.now }
+  }, options);
+  
+  // PostSchema.index({ 'updatedAt': -1, background: true });
+  
+  const PostModel = mongooseClient.model('posts', PostSchema);
 
-  // Generate Generic Post Model
-  const PostModel = mongooseClient.model<any>(modelName, PostSchema);
+  const TextPostSchema = new Schema({
+    text: { type: String, default: null }
+  }, options);
+  
+  // TextPostSchema.index({'updatedAt': -1, background: true});
+  
+  // Note the use of `Post.discriminator` rather than `mongoose.discriminator`.
+  const TextPost = PostModel.discriminator('text', TextPostSchema);
 
-  // Subschema's
-  const ImagePostSchema = new Schema({
-    image: { type: String, required: true }
-  }, {
-    discriminatorKey: '_type',
-    timestamps: true,
-  });
+  // // Generic Post Schema
+  // const PostSchema = new Schema({
+  //   text: { type: String, required: true }
+  // }, {
+  //   discriminatorKey: '_type',
+  //   timestamps: true,
+  // });
 
-  // Models for Subschema's
-  const ImagePostModel = PostModel.discriminator('image', ImagePostSchema);
+  // // Generate Generic Post Model
+  // const PostModel = mongooseClient.model<any>(modelName, PostSchema);
 
-  // This is necessary to avoid model compilation errors in watch mode
-  // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
-  if (mongooseClient.modelNames().includes(modelName)) {
-    (mongooseClient as any).deleteModel(modelName);
-  }
-  return [PostModel, ImagePostModel];
+  // // Subschema's
+  // const ImagePostSchema = new Schema({
+  //   image: { type: String, required: true }
+  // }, {
+  //   discriminatorKey: '_type',
+  //   timestamps: true,
+  // });
+
+  // // Models for Subschema's
+  // const ImagePostModel = PostModel.discriminator('image', ImagePostSchema);
+
+  // // This is necessary to avoid model compilation errors in watch mode
+  // // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
+  // if (mongooseClient.modelNames().includes(modelName)) {
+  //   (mongooseClient as any).deleteModel(modelName);
+  // }
+  // return [PostModel, ImagePostModel];
+
+  return [PostModel, TextPost];
 }
--- src/services/posts/posts.service.ts
+++ src/services/posts/posts.service.ts
@@ -4,6 +4,7 @@ import { Application } from '../../declarations';
 import { Posts } from './posts.class';
 import createModel from '../../models/posts.model';
 import hooks from './posts.hooks';
+// import service from 'feathers-mongoose';
 
 // Add this service to the service type index
 declare module '../../declarations' {
@@ -18,11 +19,18 @@ export default function (app: Application): void {
   const options = {
     Model: models[0],
     paginate: app.get('paginate'),
-    discriminators: models.slice(1)
+    discriminators: [ models[1] ]
   };
 
   // Initialize our service with any options it requires
   app.use('/posts', new Posts(options, app));
+
+  // Using the discriminators option, let feathers know about any inherited models you may have
+  // for that service
+  // app.use('/posts', service({
+  //   Model: models[0],
+  //   discriminators: [ models[1] ]
+  // }));
 
   // Get our initialized service so that we can register hooks
   const service = app.service('posts');
